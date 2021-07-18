569bba779c7f540a096636e194e4dda7b6ce9c62
Daniyal Ahmad Rizaldhi <onieln14@gmail.com>
2021-07-19 01:56:33 +0700

    update: Feed prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles, TablePagination } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { FeedItemType } from './components/FeedItem';
+import Search from './components/Search';
+import Feeds from './containers/Feeds';
+import FeedContextProvider, { FeedContext } from './contexts/FeedContext';
+
+const useStyles = makeStyles((theme) => ({
+  App: {
+    
+  },
+  Search: {
+    width: '40rem',
+    margin: '0 auto'
+  },
+  Search__Wrapper: {
+    zIndex: 1,
+    padding: '1rem 0px'
+  },
+  Feeds__Wrapper: {
+    padding: '1rem'
+  },
+  Pagination__Wrapper: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+}))
 
 function App() {
+  const styles = useStyles()
+  const [list, setList] = useState<FeedItemType[]>([])
+  const [page, setPage] = useState(0)
+  const [rowPerPage, setRowPerPage] = useState(10)
+
+  function handlePageChange (event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null, page: number) {
+    setPage(page)
+  }
+
+  function handleRowsPerPageChange (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | null) {
+    setRowPerPage(parseInt(event?.target.value || '10', 10))
+
+    setPage(0)
+  }
+
+  useEffect(() => {
+    setList([])
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FeedContextProvider value={{ images: list }}>
+      <FeedContext.Consumer>
+        {([ contextData ]) => (
+        <div className={styles.App}>
+          <div className={styles.Search__Wrapper}>
+            <Search className={styles.Search}/>
+          </div>
+          
+          <Feeds items={contextData.images}/>
+
+          <div className={styles.Pagination__Wrapper}>
+            <TablePagination component="div" count={list.length || 0} page={page} rowsPerPage={rowPerPage} onPageChange={handlePageChange} onRowsPerPageChange={handleRowsPerPageChange} rowsPerPageOptions={[5, 10, 20]}/>
+          </div>
+        </div>
+        )}
+      </FeedContext.Consumer>
+    </FeedContextProvider>
   );
 }
 
